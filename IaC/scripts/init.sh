c317510f27e4a2f80cf95346e16e6f3a679a039f
Dan <dankvitca326@gmail.com>
2025-03-07 20:08:44 +0200

    modified publish step

--- IaC/scripts/init.sh
+++ IaC/scripts/init.sh
@@ -11,9 +11,9 @@ ECR_REPOSITORY="686255960799.dkr.ecr.ap-south-1.amazonaws.com/deployment"
 # # Authenticate with ECR (IAM Role allows this without credentials)
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPOSITORY
 
-# Pull the latest image from ECR
-#docker pull $ECR_REPOSITORY:latest
-docker pull "$ECR_REPOSITORY:latest"
+# Pull the image from ECR
+$IMAGE_VERSION="1.0"
+docker pull "$ECR_REPOSITORY:$IMAGE_VERSION"
 
 # Stop and remove any existing container (if running)
 docker stop nginx-server 2>/dev/null || true
